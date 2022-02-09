4447710ba1c6a1fe121f4c2a0840da3689ce6f9f
Renilson Medeiros <50431134+malcoon@users.noreply.github.com>
2022-02-08 22:46:41 -0300

    Desafio Extra - Att

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,28 @@
-button = document.getElementById('btn-submit')
-button.addEventListener("click", submit)
-
 const returnNome = document.getElementById("returnNome")
 const returnIdade = document.getElementById("returnIdade")
 const returnSkills = document.getElementById("returnSkills")
+const returnGithub = document.getElementById("returnGithub")
+const returnTwitter = document.getElementById("returnTwitter")
+const returnInstagram = document.getElementById("returnInstagram")
+const returnLinkedin = document.getElementById("returnLinkedin")
 
 returnUsuario = document.getElementById('returnUsuario')
 returnIdadeUsuario = document.getElementById('returnIdadeUsuario')
 returnSkillsUsuario = document.getElementById('returnSkillsUsuario')
+returnGithubUsuario = document.getElementById('returnGithubUsuario')
+returnTwitterUsuario = document.getElementById('returnTwitterUsuario')
+returnInstagramUsuario = document.getElementById('returnInstagramUsuario')
+returnLinkedinUsuario = document.getElementById('returnLinkedinUsuario')
 returnUsuarioCadastrado = document.getElementById('btn-submit') 
 
 function submit() {
     nome = document.getElementById('inputNome').value
     idade = document.getElementById('inputIdade').value
     skills = document.getElementById('inputSkills').value
+    github = document.getElementById('inputGithub').value
+    twitter = document.getElementById('inputTwitter').value
+    instagram = document.getElementById('inputInstagram').value
+    linkedin = document.getElementById('inputLinkedin').value
 
     returnTitle.innerHTML = 'Usuário Cadastrado!'
 
@@ -25,4 +34,13 @@ function submit() {
 
     returnSkills.innerHTML = 'Skills:'
     returnSkillsUsuario.innerHTML = skills
+
+    returnGithub.innerHTML = 'Redes Sociais:'
+    returnGithubUsuario.innerHTML = `<a href="${github}" target="_blank"><i class="fa-brands fa-github"></i></a>, 
+                                    <a href="${twitter}" target="_blank"><i class="fa-brands fa-twitter"></i></a>,
+                                    <a href="${instagram}" target="_blank"><i class="fa-brands fa-instagram"></i></a>,
+                                    <a href="${linkedin}" target="_blank"><i class="fa-brands fa-linkedin"></i></a>`
 }
+
+button = document.getElementById('btn-submit')
+button.addEventListener("click", submit)
