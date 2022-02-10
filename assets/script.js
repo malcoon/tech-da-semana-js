93d5a64132db75ef994764144eb5b87950e87d16
Renilson Medeiros <50431134+malcoon@users.noreply.github.com>
2022-02-09 22:55:33 -0300

    Desafio Extra - Finalizado

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+const returnPerfil = document.getElementById("returnPerfil")
 const returnNome = document.getElementById("returnNome")
 const returnIdade = document.getElementById("returnIdade")
 const returnSkills = document.getElementById("returnSkills")
@@ -5,15 +6,6 @@ const returnGithub = document.getElementById("returnGithub")
 const returnTwitter = document.getElementById("returnTwitter")
 const returnInstagram = document.getElementById("returnInstagram")
 const returnLinkedin = document.getElementById("returnLinkedin")
-
-returnUsuario = document.getElementById('returnUsuario')
-returnIdadeUsuario = document.getElementById('returnIdadeUsuario')
-returnSkillsUsuario = document.getElementById('returnSkillsUsuario')
-returnGithubUsuario = document.getElementById('returnGithubUsuario')
-returnTwitterUsuario = document.getElementById('returnTwitterUsuario')
-returnInstagramUsuario = document.getElementById('returnInstagramUsuario')
-returnLinkedinUsuario = document.getElementById('returnLinkedinUsuario')
-returnUsuarioCadastrado = document.getElementById('btn-submit') 
 
 function submit() {
     nome = document.getElementById('inputNome').value
@@ -23,24 +15,26 @@ function submit() {
     twitter = document.getElementById('inputTwitter').value
     instagram = document.getElementById('inputInstagram').value
     linkedin = document.getElementById('inputLinkedin').value
-
+    perfil = document.getElementById('inputPerfil').value
+    
     returnTitle.innerHTML = 'Usuário Cadastrado!'
+
+    returnPerfilUsuario.innerHTML = `<img src="${perfil}">`
 
     returnNome.innerHTML = 'Nome:'
     returnNomeUsuario.innerHTML = `${nome}`
 
     returnIdade.innerHTML = 'Idade:'
-    returnIdadeUsuario.innerHTML = `${idade}` + ' Anos'
+    returnIdadeUsuario.innerHTML = `${idade} anos`
 
     returnSkills.innerHTML = 'Skills:'
     returnSkillsUsuario.innerHTML = `${skills}`
 
     returnGithub.innerHTML = 'Redes Sociais:'
     returnGithubUsuario.innerHTML = `<a href="${github}" target="_blank"><i class="fa-brands fa-github"></i></a>, 
-                                    <a href="${twitter}" target="_blank"><i class="fa-brands fa-twitter"></i></a>,
-                                    <a href="${instagram}" target="_blank"><i class="fa-brands fa-instagram"></i></a>,
-                                    <a href="${linkedin}" target="_blank"><i class="fa-brands fa-linkedin"></i></a>`
+                                     <a href="${twitter}" target="_blank"><i class="fa-brands fa-twitter"></i></a>,
+                                     <a href="${instagram}" target="_blank"><i class="fa-brands fa-instagram"></i></a>,
+                                     <a href="${linkedin}" target="_blank"><i class="fa-brands fa-linkedin"></i></a>`
 }
 
-button = document.getElementById('btn-submit')
-button.addEventListener("click", submit)
+button = document.getElementById('btn-submit').addEventListener("click", submit)
